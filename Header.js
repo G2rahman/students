7929ea317e9436c15fa3e67e2e82232b869ef856
G2rahman <94071678+G2rahman@users.noreply.github.com>
2022-01-31 18:32:45 -0500

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+export default function Header(){
+    return(
+        <div className="Header">
+            <h1 className="Headers">Student Data</h1>
+            <h2 className="Header">Hello</h2>
+        </div>
+    )
+}
